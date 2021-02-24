9f7c052e5e2396e6f6c00fb19dbed9cb6f844b15
Ethan Wallace <ethan.wallace.91@gmail.com>
2021-02-24 14:46:32 -0500

    Added three tests to UpdateUserHttp

---
+++ UpdateUserHttp.Tests/LocalSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace UpdateUserHttp.Tests
+{
+    class LocalSettings
+    {
+        public bool IsEncrypted { get; set; }
+        public Dictionary<string, string> Values { get; set; }
+
+
+        public static async Task SetupEnvironment()
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            string basePath = Path.GetFullPath(@"..\..\..\UpdateUserHttp");
+            try
+            {
+                var settings = JsonConvert.DeserializeObject<LocalSettings>(
+                File.ReadAllText(basePath + "\\local.settings.json"));
+
+                foreach (var setting in settings.Values)
+                {
+                    config.AppSettings.Settings.Remove(setting.Key);
+                    config.AppSettings.Settings.Add(setting.Key, setting.Value);
+                }
+                config.Save();
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+            catch (Exception e)
+            {
+                // no local.settings.json file
+            }
+                
+        }
+    }
+}
---
+++ UpdateUserHttp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UpdateUserHttp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UpdateUserHttp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("748b3b3b-8856-4262-9a96-ae6659c8967f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
